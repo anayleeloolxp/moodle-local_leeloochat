8e253804e8d0c75db73ceb07deed24b13d4e0270
anayleeloolxp <anayjain@leeloolxp.com>
2022-05-23 12:16:20 +0530

    General formatting

--- lib.php
+++ lib.php
@@ -93,14 +93,14 @@ function local_leeloochat_before_footer() {
         $leeloolxplicense = get_config('local_leeloochat')->license;
         $leeloopage = get_config('local_leeloochat')->pageurl;
 
-        $x = '<img src=\"'.new moodle_url('/local/leeloochat/icons/close.png').'\"/>';
-        $f = '<img src=\"'.new moodle_url('/local/leeloochat/icons/column.png').'\"/>';
-        $p = '<img src=\"'.new moodle_url('/local/leeloochat/icons/full-page.png').'\"/>';
-        $w = '<img src=\"'.new moodle_url('/local/leeloochat/icons/widget.png').'\"/>';
+        $x = '<img src=\"' . new moodle_url('/local/leeloochat/icons/close.png') . '\"/>';
+        $f = '<img src=\"' . new moodle_url('/local/leeloochat/icons/column.png') . '\"/>';
+        $p = '<img src=\"' . new moodle_url('/local/leeloochat/icons/full-page.png') . '\"/>';
+        $w = '<img src=\"' . new moodle_url('/local/leeloochat/icons/widget.png') . '\"/>';
 
         $pgbtn = '';
         if ($leeloopage != '') {
-            $pgbtn = '<span onclick=\"show_page(\'' . $leeloopage . '\');\">'.$p.'</span>';
+            $pgbtn = '<span onclick=\"show_page(\'' . $leeloopage . '\');\">' . $p . '</span>';
         }
 
         $PAGE->requires->js('/local/leeloochat/js/widget.js');
@@ -111,9 +111,9 @@ function local_leeloochat_before_footer() {
 
         $wespherchattitle = '<span class=\"wespher_chat_title\" onclick=\"show_full();\">' . get_string('widget_title', 'local_leeloochat') . '</span>';
 
-        $closeframe = '<span onclick=\"close_frame();\">'.$x.'</span>';
+        $closeframe = '<span onclick=\"close_frame();\">' . $x . '</span>';
 
-        $btns = '<div class=\"wespherbuttonsdiv\"><span onclick=\"show_half();\">'.$w.'</span><span onclick=\"show_full();\">'.$f.'</span>' . $pgbtn . $closeframe . '</div>';
+        $btns = '<div class=\"wespherbuttonsdiv\"><span onclick=\"show_half();\">' . $w . '</span><span onclick=\"show_full();\">' . $f . '</span>' . $pgbtn . $closeframe . '</div>';
 
         $frame = '<iframe id=\"wespher_widget_frame\" class=\"wespher_widget\" src=\"' . $frameurl . '\"></iframe>';
 
@@ -124,12 +124,12 @@ function local_leeloochat_before_footer() {
 
         z.innerHTML = "<div class=\"wespher_chat\">' . $wespherchattitle . $btns . '</div>' . $frame . '";
         document.body.appendChild(z);
-        
+
         var navleeloolxpchatcontainer = document.getElementById("navleeloolxpchatcontainer");
         if(navleeloolxpchatcontainer){
             document.getElementById("navleeloolxpchatcontainer").classList.remove("l_hidden");
         }
-        
+
         ';
 
         $PAGE->requires->js_init_code("$js2");
